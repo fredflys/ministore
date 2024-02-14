9f3a4f8f6e856e1fe51a76fb057c47d084fe118a
fredflys <xyf220@126.com>
2024-02-14 13:02:07 -0800

    added task list

---
+++ app.js
@@ -0,0 +1,12 @@
+const switcher = document.querySelector(".btn");
+switcher.addEventListener("click", function () {
+  document.body.classList.toggle("light-theme");
+  document.body.classList.toggle("dark-theme");
+});
+
+// const currentTheme = document.body.className;
+// if (className == "light-theme") {
+//   this.textContent = "Dark";
+// } else {
+//   this.textContent = "Light";
+// }
